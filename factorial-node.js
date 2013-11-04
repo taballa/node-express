9cd3559d55446dd81ee3d7a80a79ef4ccf997040
taballa.hp-PD <taballa@gmail.com>
2013-11-04 22:14:33 +0800

    update factorial-node.js page

--- factorial-node.js
+++ factorial-node.js
@@ -1,1 +1,21 @@
 var htutil = require("./htutil.js")
+var math = require("./math")
+
+exports.get = function(req, res){
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.end(
+        htutil.page("Factorial", htutil.navbar(), [
+            (!isNaN(req.a) ?
+                ('<p class="factorial">{a} factorial = {fact}</p>'
+                    .replace("{a}", req.a)
+                    .replace("{fact}", math.factorial(Math.floor(req.a)))
+                )
+                : ""
+            ),
+            "<p>Enter a number to see it's factorial. </p>",
+            '<form action="/factorial" method="get" name="factorial">',
+            'A: <input type="text" name="factorial" />',
+            '</form>'
+            ].join('\n'))
+        )
+}
