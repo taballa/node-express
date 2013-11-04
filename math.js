62030ffe50b4502ca10f1aa093d3877b5ae41f91
taballa.hp-PD <taballa@gmail.com>
2013-11-05 01:01:06 +0800

    fibonacciAsync

--- math.js
+++ math.js
@@ -11,5 +11,21 @@ var fibonacci = exports.fibonacci = function(n){
     else if (n === 2)
         return 1;
     else
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2);
+}
+
+var fibonacciAsync = exports.fibonacciAsync = function(n, done){
+    if (n ===1 || n === 2)
+        done(1);
+    else {
+        process.nextTick(function(){
+            fibonacciAsync(n-1, function(val1){
+                process.nextTick(function(){
+                    fibonacciAsync(n-2, function(val2){
+                        done(val1+val2);
+                    })
+                })
+            })
+        })
+    }
 }
