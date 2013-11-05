af4c97d8d39cee9b0ec0a6d7e2a0a92a085a0ab6
taballa.hp-PD <taballa@gmail.com>
2013-11-05 17:03:36 +0800

    update fibo2-node.js, use function sendResult(req, res, a, fiboval)

--- fibo2-node.js
+++ fibo2-node.js
@@ -1,16 +1,35 @@
 var htutil = require("./htutil")
 var math = require('./math')
+
+function sendResult(req, res, a, fiboval){
+    res.writeHead(200, {
+        'Content-Type':'text/html'
+    })
+    res.end(
+        htutil.page(
+            "Fibonacci",
+            htutil.navbar,
+            [
+                (!isNaN(fiboval)?
+                    '<p class="result">fibonacci {a} = {fibo}</p>'
+                    .replace("{a}", req.a)
+                    .replace("{fibo}", fiboval)
+                :""),
+                '<p>Enter a number to see its fibonacci.</p>',
+                '<form action="/fibo2" method="get" name="fibonacci">',
+                'A: <input type="text" name="a" />',
+                '</form>'
+            ].join('\n')
+            )
+        )
+}
+
 exports.get = function(req, res){
-    res.writeHead(200, {"Content-Type":"text/html"})
-    res.end(htutil.page("Fibonacci", htutil.navbar, [
-        (!isNaN(req.a) ? (
-            '<p class="fibonacci">fibonacci {a} = {fibo}</p>'
-            .replace("{a}", Math.floor(req.a))
-            .replace("{fibo}", math.fibonacciAsync(Math.floor(req.a)))
-            ) : ""),
-        '<p>Enter a number to see its fibonacci.</p>',
-        '<form action="/fibonacci" method="get" name="fibonacci">',
-        'A: <input type="text" name="a" />',
-        '</form>'
-        ].join('\n')))
+    if (!isNaN(req.a)){
+        math.fibonacciAsync(Math.floor(req.a), function(val){
+            sendResult(req, res, Math.floor(req.a), val);
+        })
+    } else {
+        sendResult(req, res, NaN, NaN)
+    }
 }
