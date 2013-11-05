33abda3059e29e81c9f3589cd01c6975c3545e58
taballa.hp-PD <taballa@gmail.com>
2013-11-05 17:01:15 +0800

    process.nextTick => setImmediate

--- math.js
+++ math.js
@@ -18,9 +18,9 @@ var fibonacciAsync = exports.fibonacciAsync = function(n, done){
     if (n ===1 || n === 2)
         done(1);
     else {
-        process.nextTick(function(){
+        setImmediate(function(){
             fibonacciAsync(n-1, function(val1){
-                process.nextTick(function(){
+                setImmediate(function(){
                     fibonacciAsync(n-2, function(val2){
                         done(val1+val2);
                     })
