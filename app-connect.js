945194a36f73d00ae9e10477484b3f9b58adf6e9
taballa.hp-PD <taballa@gmail.com>
2013-11-06 15:33:09 +0800

    require 'connect-route'

--- app-connect.js
+++ app-connect.js
@@ -1,13 +1,18 @@
 var connect = require('connect')
+var connectRoute = require('connect-route')
 var htutil = require('./htutil')
 
 connect.createServer()
     .use(connect.favicon())
     .use(connect.logger())
     .use('/filez', connect.static(__dirname + 'filez'))
-    .use(connect.router(function(app){
+    .use(connectRoute(function(app){
         app.get('/', require('./home-node').get);
-        app.get('/square', htutil.loadParams, require('./square-node').get)
+        // Fix: htutil.loadParams 在这里执行会引起错误，先去试试 expressjs
+        // app.get('/square', htutil.loadParams, require('./square-node').get);
+        // app.get('/factorial', htutil.loadParams, require('./factorial-node').get);
+        // app.get('/fibonacci', htutil.loadParams, require('./fibo2-node').get);
+    })).listen(8124)
 
-    }))
+console.log('listening to http://127.0.0.1:8124')
 
