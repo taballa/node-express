f35da201beb8fcf6d865840f90aa816a7dbaf593
taballa-hp-mini <taballa@gmail.com>
2013-11-17 20:14:05 +0800

    test1 -> test2

--- test/test.js
+++ test/test.js
@@ -1,35 +1,29 @@
-var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
-})
+require("should");
 
-describe('User', function(){
-  describe('#save()', function(){
-    it('should save without error', function(done){
-      var user = new User('Luna');
-      user.save(function(err){
-        if (err) throw err;
-        done();
-      });
-    })
-  })
-})
+var name = "zhaojian";
 
-fs = require('fs');
-describe('File', function(){
-    describe('#readFile()', function(){
-        it.skip('should read test.ls without error', function(done){
-            fs.readFile('test.as', function(err){
-                if (err) throw err;
-                done();
-            });
-        })
-        it('should read test.js without error', function(done){
-        })
-    })
-})
+describe("Name", function() {
+    it("The name should be zhaojian", function() {
+        name.should.eql("zhaojian");
+    });
+});
+
+var Person = function(name) {
+    this.name = name;
+};
+var zhaojian = new Person(name);
+
+describe("InstanceOf", function() {
+    it("Zhaojian should be an instance of Person", function() {
+        zhaojian.should.be.an.instanceof(Person);
+    });
+
+    it("Zhaojian should be an instance of Object", function() {
+        zhaojian.should.be.an.instanceof(Object);
+    });
+});
+describe("Property", function() {
+    it("Zhaojian should have property name", function() {
+        zhaojian.should.have.property("name");
+    });
+});
