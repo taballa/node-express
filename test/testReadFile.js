e4dedd868ce81072fcb9b124fd554ba9559145cd
taballa-hp-mini <taballa@gmail.com>
2013-11-23 21:47:05 +0800

    fix testReadFile.js

--- test/testReadFile.js
+++ test/testReadFile.js
@@ -4,7 +4,9 @@ require("should");
 describe("readFile", function() {
     it("The file content should be zhaojian", function(done) {
         fs.readFile("text.txt", "utf8", function(err, data) {
-            data.should.eql("zhaojian");
+            if (err) return done(err)
+            // console.log(data)
+            data.should.eql("zhaojian\n");
             done();
         });
     });
