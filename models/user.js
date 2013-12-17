e558e532b10fdca1e0a4dfbdae7e383b1d861d0a
taballa.hp-PD <taballa@gmail.com>
2013-12-17 17:05:38 +0800

    added ./module/user.js (User.prototype.save)

--- models/user.js
+++ models/user.js
@@ -7,3 +7,37 @@ function User(user){
 }
 
 module.exports = User;
+
+
+// Save user infomation
+User.prototype.save = function(callback){
+    // user infomation
+    var user = {
+        name: this.name,
+        password: this.password,
+        email: this.email
+    }
+
+    // open database
+    mongodb.open(function(err, db){
+        if (err){
+            return callback(err) //error message
+        }
+        // reading users
+        db.collection('users', function(err, collection){
+            if (err){
+                mongodb.close();
+                return callback(err);
+            }
+            collection.insert(user, {safe: true}, function(err, user){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+
+                }
+                callback(null, user[0]); // success! return queried user infomation
+            })
+        })
+    })
+}
+
