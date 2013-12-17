4a78bfbaa9061e0cc00ff59bed1f78a2f8cd8058
taballa.hp-PD <taballa@gmail.com>
2013-12-17 17:06:31 +0800

    module/user.js User.get

--- models/user.js
+++ models/user.js
@@ -41,3 +41,29 @@ User.prototype.save = function(callback){
     })
 }
 
+// Get user infomation
+User.get = function(name, callback) {
+  //打开数据库
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);//错误，返回 err 信息
+    }
+    //读取 users 集合
+    db.collection('users', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);//错误，返回 err 信息
+      }
+      //查找用户名（name键）值为 name 一个文档
+      collection.findOne({
+        name: name
+      }, function (err, user) {
+        mongodb.close();
+        if (err) {
+          return callback(err);//失败！返回 err
+        }
+        callback(null, user);//成功！返回查询的用户信息
+      });
+    });
+  });
+};
