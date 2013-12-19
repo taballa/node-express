3a80717cc5c11305297bf35294a9ddcf993a90b3
taballa.hp-PD <taballa@gmail.com>
2013-12-19 18:02:31 +0800

    prepare /post

---
+++ models/post.js
@@ -0,0 +1,77 @@
+var mongodb = require('./db');
+
+function Post(name, title, post){
+    this.name = name;
+    this.title = title;
+    this.post = post;
+}
+
+module.exports = Post;
+
+// save article
+Post.prototype.save = function(callback){
+    var date = new Date();
+
+    // all kinds of time.
+    var time = {
+        date : date,
+        year : date.getFullYear(),
+        month : date.getFullYear() + '-' + (date.getMonth() + 1),
+        day : date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate(),
+        minute : date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getHours() + ':' + (date.getMinutes() < 10 : '0' + date.getMinutes() : date.getMinutes())
+    }
+
+    // article
+    var post = {
+        name = this.name;
+        time = time;
+        title = this.title;
+        post = this.post;
+    }
+
+    // open database
+    mongodb.open(function(err, db){
+        if (err){
+            return callback(err)
+        }
+        db.collection('posts', function(err, callback){
+            if (err){
+                mongodb.close();
+                return callback(err);
+            }
+            // article insert to posts collection
+            collection.insert(post, {
+                saft: true
+            }, function(err){
+                mongodb.close();
+                if (err){
+                    return callback(err)
+                }
+                callback(null)
+            })
+        })
+    })
+}
+
+// reading article
+Post.get = function(name, callback){
+    // open database
+    mongodb.open(function(err, db){
+        if (err){
+            return callback(err)
+        }
+        // reading posts collection
+        db.collection('posts', function(err, callback){
+            if (err){
+                mongodb.close();
+                return callback(err);
+            }
+            var query = {};
+            if (name){
+                query.name = name;
+            }
+            // search article of query.name
+            collection.find(query).sort()
+        })
+    })
+}
