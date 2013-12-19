3d5422bc8f71b25329402d730f7c2e160df035e1
taballa.hp-PD <taballa@gmail.com>
2013-12-19 18:04:21 +0800

    prepare /users

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,15 @@
-
 /*
  * GET users listing.
  */
 
-exports.list = function(req, res){
-  res.send("respond with a resource");
+exports.list = function(req, res) {
+    // res.send("respond with a resource");
+    console.log('user:' + req.seesion)
+    // if (!req.seesion.user) {
+    //     req.flash('error', 'not login')
+    //     res.redirect('/login')
+    // }
+    res.render('user', {
+        user: req.session.user
+    })
 };
